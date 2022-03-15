76d06fa669aeea8dcc1a3de67b7ec093f7fe043b
Walid Arnous <walidi1@hotmail.com>
2022-03-15 13:14:32 -0300

    Parte 8 - Avançado 1

---
+++ Exercicios_Desafios/8Person.rb
@@ -0,0 +1,31 @@
+module Person
+    class Juridic
+        def initialize(nome, cnpj)
+            @nome = nome
+            @cnpj = cnpj
+        end
+    
+        def add
+            puts "Pessoa jurídica adicionada"
+            puts "nome: #{@nome}"
+            puts "cnpj: #{@cnpj}"
+        end
+    end
+    
+    
+    class Physical
+        def initialize(nome, cpf)
+            @nome = nome
+            @cpf = cpf
+        end
+    
+        def add
+            puts "Pessoa física adicionada"
+            puts "nome: #{@nome}"
+            puts "cpf: #{@cpf}"
+        end
+    end
+end
+    
+    Person::Juridic.new('M. C. Investimentos', '4241.123/0001').add
+    Person::Physical.new('José Almeida', '425.123.123-123').add
