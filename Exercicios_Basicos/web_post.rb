bf3d4307af2d766acccb33548268978165927262
Walid Arnous <walidi1@hotmail.com>
2022-03-17 17:09:20 -0300

    parte 9 - chamadas web e arquivo

---
+++ Exercicios_Basicos/web_post.rb
@@ -0,0 +1,13 @@
+require 'net/http'
+ 
+req = Net::HTTP::Post.new("/api/users")
+# para fazer chamadas https
+req.set_form_data({ name:'Mario', job:'Encanador' })
+ 
+response = Net::HTTP.start('reqres.in', use_ssl: true) do |https|
+ https.request(req)
+end
+ 
+puts response.message
+puts response.code
+puts response.body
